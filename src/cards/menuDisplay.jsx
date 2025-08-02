6baa565d9f6e7282e3f074dae319983c426fcc98
Yug Bhatia <bhatiayug175@gmail.com>
2025-08-02 13:46:51 +0530

    Veg Non veg sorting is now working

--- src/cards/menuDisplay.jsx
+++ src/cards/menuDisplay.jsx
@@ -1,10 +1,190 @@
 import { useState } from "react";
 import { ChevronUp, ChevronDown } from "lucide-react";
-
-export default function MenuDisplay({ item }) {
-  console.log(item) ; 
+export default function MenuDisplay({ item, foodSelected }) {
   const [open, setOpen] = useState(true);
-
+  if (foodSelected == "veg" ) {
+    return (
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between w-[100%]">
+          <h2 className="text-xl font-semibold">{item.title}</h2>
+          <button onClick={() => setOpen(!open)}>
+            {open ? <ChevronUp /> : <ChevronDown />}
+          </button>
+        </div>
+        {!open && <div className="w-full h-[10px] bg-gray-100"></div>}
+        {open && (
+          <>
+            {item?.carousel
+              ? item?.carousel?.
+              filter((dish) => dish?.dish?.info?.isVeg).
+              map((dish) => (
+                  <div
+                    key={dish?.dish?.info?.id}
+                    className="flex items-center justify-between p-2 border-b border-gray-200"
+                  >
+                    <div className="w-[70%] flex flex-col">
+                      <h3 className="text-lg font-medium">
+                        {dish?.dish?.info?.name}
+                      </h3>
+                      <p className="text-sm text-gray-600 font-semibold">
+                        {dish?.dish?.info?.description}
+                      </p>
+                      <span className="text-sm text-gray-500">
+                        ₹
+                        {(dish?.dish?.info?.price ||
+                          dish?.dish?.info?.defaultPrice) / 100}
+                      </span>
+                    </div>
+                    <div className="w-[30%] flex justify-end flex-col items-center gap-4">
+                      <img
+                        src={
+                          dish?.dish?.info?.imageId
+                            ? `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${dish?.dish?.info?.imageId}`
+                            : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/11/29/dc90cc06-f883-430d-be4b-75141c8064d9_6b832721-694c-4df4-96f0-308388144da0.jpg"
+                        }
+                        alt={dish?.dish?.info?.name}
+                        className="w-25 h-25 rounded-md object-cover"
+                      />
+                      <button className="w-30 bg-orange-500 text-white px-4 py-2 rounded-md ml-4">
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                ))
+              : 
+              item?.itemCards?.
+              filter((dish) => dish?.card?.info?.isVeg).
+              map((dish) => (
+                  <div
+                    key={dish?.card?.info?.id}
+                    className="flex items-center justify-between p-2 border-b border-gray-200"
+                  >
+                    <div className="w-[70%] flex flex-col">
+                      <h3 className="text-lg font-medium">
+                        {dish?.card?.info?.name}
+                      </h3>
+                      <p className="text-sm text-gray-600 font-semibold">
+                        {dish?.card?.info?.description}
+                      </p>
+                      <span className="text-sm text-gray-500">
+                        ₹
+                        {(dish?.card?.info?.price ||
+                          dish?.card?.info?.defaultPrice) / 100}
+                      </span>
+                    </div>
+                    <div className="w-[30%] flex justify-end flex-col items-center gap-4">
+                      <img
+                        src={
+                          dish?.card?.info?.imageId
+                            ? `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${dish?.card?.info?.imageId}`
+                            : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/11/29/dc90cc06-f883-430d-be4b-75141c8064d9_6b832721-694c-4df4-96f0-308388144da0.jpg"
+                        }
+                        alt={dish?.card?.info?.name}
+                        className="w-25 h-25 rounded-md object-cover"
+                      />
+                      <button className="w-30 bg-orange-500 text-white px-4 py-2 rounded-md ml-4">
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                ))}
+          </>
+        )}
+      </div>
+    );
+  }
+   if (foodSelected == "non-veg" ) {
+    return (
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center justify-between w-[100%]">
+          <h2 className="text-xl font-semibold">{item.title}</h2>
+          <button onClick={() => setOpen(!open)}>
+            {open ? <ChevronUp /> : <ChevronDown />}
+          </button>
+        </div>
+        {!open && <div className="w-full h-[10px] bg-gray-100"></div>}
+        {open && (
+          <>
+            {item?.carousel
+              ? item?.carousel?.
+              filter((dish) => !dish?.dish?.info?.isVeg).
+              map((dish) => (
+                  <div
+                    key={dish?.dish?.info?.id}
+                    className="flex items-center justify-between p-2 border-b border-gray-200"
+                  >
+                    <div className="w-[70%] flex flex-col">
+                      <h3 className="text-lg font-medium">
+                        {dish?.dish?.info?.name}
+                      </h3>
+                      <p className="text-sm text-gray-600 font-semibold">
+                        {dish?.dish?.info?.description}
+                      </p>
+                      <span className="text-sm text-gray-500">
+                        ₹
+                        {(dish?.dish?.info?.price ||
+                          dish?.dish?.info?.defaultPrice) / 100}
+                      </span>
+                    </div>
+                    <div className="w-[30%] flex justify-end flex-col items-center gap-4">
+                      <img
+                        src={
+                          dish?.dish?.info?.imageId
+                            ? `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${dish?.dish?.info?.imageId}`
+                            : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/11/29/dc90cc06-f883-430d-be4b-75141c8064d9_6b832721-694c-4df4-96f0-308388144da0.jpg"
+                        }
+                        alt={dish?.dish?.info?.name}
+                        className="w-25 h-25 rounded-md object-cover"
+                      />
+                      <button className="w-30 bg-orange-500 text-white px-4 py-2 rounded-md ml-4">
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                ))
+              : 
+              item?.itemCards?.
+              filter((dish) => !dish?.card?.info?.isVeg).
+              map((dish) => (
+                  <div
+                    key={dish?.card?.info?.id}
+                    className="flex items-center justify-between p-2 border-b border-gray-200"
+                  >
+                    <div className="w-[70%] flex flex-col">
+                      <h3 className="text-lg font-medium">
+                        {dish?.card?.info?.name}
+                      </h3>
+                      <p className="text-sm text-gray-600 font-semibold">
+                        {dish?.card?.info?.description}
+                      </p>
+                      <span className="text-sm text-gray-500">
+                        ₹
+                        {(dish?.card?.info?.price ||
+                          dish?.card?.info?.defaultPrice) / 100}
+                      </span>
+                    </div>
+                    <div className="w-[30%] flex justify-end flex-col items-center gap-4">
+                      <img
+                        src={
+                          dish?.card?.info?.imageId
+                            ? `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${dish?.card?.info?.imageId}`
+                            : "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/11/29/dc90cc06-f883-430d-be4b-75141c8064d9_6b832721-694c-4df4-96f0-308388144da0.jpg"
+                        }
+                        alt={dish?.card?.info?.name}
+                        className="w-25 h-25 rounded-md object-cover"
+                      />
+                      <button className="w-30 bg-orange-500 text-white px-4 py-2 rounded-md ml-4">
+                        Add
+                      </button>
+                    </div>
+                  </div>
+                ))}
+          </>
+        )}
+      </div>
+    );
+  }
+  console.log(item);
   return (
     <div className="flex flex-col gap-2">
       <div className="flex items-center justify-between w-[100%]">
@@ -13,7 +193,7 @@ export default function MenuDisplay({ item }) {
           {open ? <ChevronUp /> : <ChevronDown />}
         </button>
       </div>
-       {!open && <div className="w-full h-[10px] bg-gray-100"></div>}
+      {!open && <div className="w-full h-[10px] bg-gray-100"></div>}
       {open && (
         <>
           {item?.carousel
@@ -30,7 +210,9 @@ export default function MenuDisplay({ item }) {
                       {dish?.dish?.info?.description}
                     </p>
                     <span className="text-sm text-gray-500">
-                      ₹{(dish?.dish?.info?.price || dish?.dish?.info?.defaultPrice) / 100}
+                      ₹
+                      {(dish?.dish?.info?.price ||
+                        dish?.dish?.info?.defaultPrice) / 100}
                     </span>
                   </div>
                   <div className="w-[30%] flex justify-end flex-col items-center gap-4">
@@ -62,7 +244,9 @@ export default function MenuDisplay({ item }) {
                       {dish?.card?.info?.description}
                     </p>
                     <span className="text-sm text-gray-500">
-                      ₹{(dish?.card?.info?.price || dish?.card?.info?.defaultPrice) / 100}
+                      ₹
+                      {(dish?.card?.info?.price ||
+                        dish?.card?.info?.defaultPrice) / 100}
                     </span>
                   </div>
                   <div className="w-[30%] flex justify-end flex-col items-center gap-4">
--- src/cards/menuItems.jsx
+++ src/cards/menuItems.jsx
@@ -1,12 +1,10 @@
 import MenuDisplay from "./menuDisplay";
 import { useState } from "react";
-import { ChevronDown } from "lucide-react";
-import { ChevronUp } from "lucide-react";
 export default function MenuItems({ menuItems }) {
   console.log(menuItems[0]?.title);
   console.log(menuItems[1]?.itemCards);
   console.log(menuItems);
-  const [open, setOpen] = useState(true);
+  const [selectedOption , setSelectedOption] = useState(null);
   if (!menuItems || menuItems.length === 0) {
     return (
       <div
@@ -17,13 +15,50 @@ xl p-6 flex flex-col gap-3 border border-gray-200"
       </div>
     );
   }
+  function handleSelected(option) {
+    if(selectedOption === option){
+      setSelectedOption(null);
+    }
+    if(option=== "veg"){
+      setSelectedOption("veg");
+    }
+    if(option=== "non-veg"){
+      setSelectedOption("non-veg");
+    }
+  }
   return (
     <div className="w-[70%] bg-white shadow-md rounded-3xl p-6 flex flex-col gap-3 border border-gray-200">
+      <div className="flex gap-4 mt-4">
+      <button
+        className={`px-4 py-2 rounded-full text-sm font-medium border transition-all duration-300 
+        ${
+          selectedOption === "veg"
+            ? "bg-green-100 text-green-700 border-green-500 shadow-md"
+            : "text-gray-500 border-gray-300 hover:border-green-400 hover:text-green-600"
+        }`}
+        onClick={() => handleSelected("veg")}
+      >
+        🌱 Veg
+      </button>
+
+      <button
+        className={`px-4 py-2 rounded-full text-sm font-medium border transition-all duration-300 
+        ${
+          selectedOption === "non-veg"
+            ? "bg-red-100 text-red-700 border-red-500 shadow-md"
+            : "text-gray-500 border-gray-300 hover:border-red-400 hover:text-red-600"
+        }`}
+        onClick={() => handleSelected("non-veg")}
+      >
+        🍗 Non-Veg
+      </button>
+    </div>
       {menuItems?.map((item, index) => {
         return (
             <MenuDisplay
               key={index}
               item={item}
+              foodSelected={selectedOption}
             />
         );
       })}
